0d0d6edabf234eb0683aed0eecf78e8d2e5a9838
icaro paiva <icaro.paiva@acct.global>
2022-07-19 23:27:44 -0300

    requests methods with query and body

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i0.wp.com"],
-    formats: ["image/webp"]
+    domains: ["i0.wp.com"]
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "*" }
+        ]
+      }
+    ]
   }
 }
 
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,8 +1,38 @@
 import { NextApiHandler } from "next"
 import { users } from "../../../utils/users"
 
-const handler: NextApiHandler = (_, res) => {
+// get users
+const handlerGET: NextApiHandler = (req, res) => {
+  const { limit } = req.query
+  const limitNumber = Number(limit)
+
+  if (limit && !isNaN(limitNumber)) {
+    const usersLimiteds = users.filter((_, index) => index + 1 <= limitNumber)
+
+    res.json(usersLimiteds)
+    return
+  }
+
   res.json(users)
+}
+
+// insert new user
+const handlerPOST: NextApiHandler = (req, res) => {
+  const { name, age } = req.body
+  res.json({ id: 4, name, age })
+}
+
+const handler: NextApiHandler = (req, res) => {
+  switch (req.method) {
+    case "GET":
+      handlerGET(req, res)
+      break
+    case "POST":
+      handlerPOST(req, res)
+      break
+    default:
+      res.json({ type: "another method" })
+  }
 }
 
 export default handler
